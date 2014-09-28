16c99fd3acc8a6f3e0954521e522507fe28691d2
Matthew Mosley <mdmosley1@gmail.com>
2014-09-28 13:35:56 -0500

    added picture

--- Matlab/Base.m
+++ Matlab/Base.m
@@ -1,7 +1,7 @@
 classdef Base < Component
 % BASE This is the base of the Lynx robot arm
     properties
-        mR, mH, angle, 
+        mR, mH, mAngle, 
     end
     
     methods
@@ -10,13 +10,13 @@ classdef Base < Component
             obj.mR = r;
             obj.mH = h;
             obj.mVnum = 50; % 50 vertices on bottom
-            obj.angle = 360 / obj.mVnum;
+            obj.mAngle = 360 / obj.mVnum;
 
             for i=1:obj.mVnum
-                obj.mVertices(i,:) = [obj.mR*cosd((i-1)*obj.angle), obj.mR*...
-                                    sind((i-1)*obj.angle), 0]; % lower v's
-                obj.mVertices(i+obj.mVnum,:) = [obj.mR*cosd((i-1)*obj.angle),...
-                                    obj.mR*sind((i-1)*obj.angle), obj.mH]; % upper v's
+                obj.mVertices(i,:) = [obj.mR*cosd((i-1)*obj.mAngle), obj.mR*...
+                                    sind((i-1)*obj.mAngle), 0]; % lower v's
+                obj.mVertices(i+obj.mVnum,:) = [obj.mR*cosd((i-1)*obj.mAngle),...
+                                    obj.mR*sind((i-1)*obj.mAngle), obj.mH]; % upper v's
                 obj.updateFaces();
             end
         end
--- Matlab/main.m
+++ Matlab/main.m
@@ -9,15 +9,15 @@ base2 = Base(r2,tk); base2.translate(r1,r1,tk+sep); base2.build('b');
 
 splate1 = Servo(1.7,61,34.8); splate1.translate(12.1,11.5,tk*2+sep); splate1.build('y');
 
-servo1 = Servo(40.3,42.2,19.9);servo1.translate(-14.9,17.8,tk*2+sep+9.3);servo1.build('y');
+servo1 = Servo(40.3-10,42.2,19.9);servo1.translate(-14.9,17.8,tk*2+sep+9.3);servo1.build('y');
 
 splate2 = Servo(1.7,61,34.8); splate2.translate(49+12.1+1.7,11.5,tk*2+sep);splate2.build('g');
 
-servo2 = Servo(40.3,42.2,19.9); servo2.translate(30.7,17.8,tk*2+sep+9.3);servo2.build('g');
+servo2 = Servo(40.3-8,42.2,19.9); servo2.translate(30.7,17.8,tk*2+sep+9.3);servo2.build('g');
 
-bicep1 = Servo(96.9+8.4*2,8.4*2,3.1); bicep1(translate)
+% bicep1 = Servo(96.9+8.4*2,8.4*2,3.1); bicep1(translate)
 
-bicep2 = Servo(96.9+8.4*2,8.4*2,3.1);
+% bicep2 = Servo(96.9+8.4*2,8.4*2,3.1);
 
 
 daspect([max(daspect)*[1 1 1]]);
