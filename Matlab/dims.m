9785809dfb7488ffbd3d3558b657b3bb50559d52
Matthew Mosley <mdmosley1@gmail.com>
2014-10-09 19:56:14 -0500

    biceps now in correct place

--- Matlab/dims.m
+++ Matlab/dims.m
@@ -3,8 +3,26 @@
 r1 = 49.07; tk = 3.1;
 r2 = 44.32; sep = 9.9;
 
-
+% size of components
 bicDim = [tk 119.4 24.5]; %bicep
 plateDim = [1.7 61 34.8]; % 
-serDim = [40.3 42.2 19.9]; %servo
+serDim = [36 40.3 19.9]; %servo
 
+% measurements in x
+x1=5;x2=10.8;x4=1.5;x5=21.2;x6=5;x7=5; x8=47.7;x9=6.9;x10=15.9;x11=50.7
+
+% measurements in y
+y1=18.3;y2=14.8;
+
+% measurements in z
+z1=10;z2=9.3;z3=7.8;
+
+% positions of components
+
+ser1Pos = [-14.9,17.8,tk*2+sep+9.3];
+splate1Pos = [12.1,11.5,tk*2+sep];
+bic1Pos = splate1Pos + [x4 0 0] + [x2 y1 z2];
+
+ser2Pos = ser1Pos + [serDim(1) 0 0] + [x10 0 0];
+splate2Pos = splate1Pos + [x11 0 0];
+bic2Pos = splate2Pos + [x2+x4 y1 z2];
--- Matlab/main.m
+++ Matlab/main.m
@@ -3,17 +3,16 @@ clear; close all; clear classes;
 % load dimensions
 dims
 
+base1 = Cyl(r1,tk); base1.translate([r1,r1,0]); base1.build('r');
+base2 = Cyl(r2,tk); base2.translate([r1,r1,tk+sep]); base2.build('b');
 
-base1 = Cyl(r1,tk); base1.translate(r1,r1,0); base1.build('r');
-base2 = Cyl(r2,tk); base2.translate(r1,r1,tk+sep); base2.build('b');
+splate1 = Block(plateDim); splate1.translate(splate1Pos); splate1.build('y');
+servo1=Block(serDim);servo1.translate(ser1Pos);servo1.build('y');
 
-splate1 = Block(plateDim); splate1.translate(12.1,11.5,tk*2+sep); splate1.build('y');
-servo1=Block(serDim);servo1.translate(-14.9,17.8,tk*2+sep+9.3);servo1.build('y');
+splate2 = Block(plateDim); splate2.translate(splate2Pos);splate2.build('g');
+servo2 = Block(serDim); servo2.translate(ser2Pos);servo2.build('g');
 
-splate2 = Block(plateDim); splate2.translate(49+12.1+1.7,11.5,tk*2+sep);splate2.build('g');
-servo2 = Block(serDim); servo2.translate(30.7,17.8,tk*2+sep+9.3);servo2.build('g');
-
-bicep1 = Block(bicDim); bicep1.build('r');%bicep1(translate)
-bicep2 = Block(bicDim); bicep2.build('r');%bicep1(translate)
+bicep1 = Block(bicDim); bicep1.translate(bic1Pos); bicep1.build('r');%bicep1(translate)
+bicep2 = Block(bicDim); bicep2.translate(bic2Pos); bicep2.build('r');%bicep1(translate)
 
 daspect([max(daspect)*[1 1 1]]);
