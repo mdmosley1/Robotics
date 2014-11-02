fdb0f6dff391ed9cbb9b98d423f8bfd856861bbd
Matthew Mosley <mdmosley1@gmail.com>
2014-11-02 15:15:48 -0600

    added gripper class

---
+++ Matlab/Gripper.m
@@ -0,0 +1,26 @@
+classdef Gripper < Component
+% GRIPPER This is a servo from the Lynx robot
+    properties
+        name,pos
+        joint = [2.3,2.3,0];
+    end
+
+    methods    
+        % class constructor
+        function obj = Gripper()
+            obj.mVnum = 5; % 4 vertices
+            tk = 3.1;
+            obj.mVertices = [0 0 0;
+                             4.6 0 0;
+                             9.5 22.1 0;
+                             4.8 43.2 0;
+                             0 42.3 0;
+                             0 0 tk;
+                             4.6 0 tk;
+                             9.5 22.1 tk;
+                             4.8 43.2 tk;
+                             0 42.3 tk];
+            obj.updateFaces();
+        end
+    end
+end
